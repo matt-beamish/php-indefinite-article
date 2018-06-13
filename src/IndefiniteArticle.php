2ca9f3700b9af145d2e353cd29dc1911f8f488b3
Spenser Hale <spenserhale@outlook.com>
2018-06-13 15:29:31 -0700

    refactoring _indefinite_article down to 3 returns, and trimming 42 lines off class

--- src/IndefiniteArticle.php
+++ src/IndefiniteArticle.php
@@ -3,6 +3,39 @@
 class IndefiniteArticle
 {
 
+    private static $rules = [
+        // any number starting with an '8' uses 'an'
+        ["/^[8](\d+)?/", 0],
+        // ordinal forms
+        ['/^([bcdgjkpqtuvwyz]-?th)/i', 1],
+        ['/^([aefhilmnorsx]-?th)/i', 0],
+        // special cases
+        ['/^(euler|hour(?!i)|heir|honest|hono)/i', 0],
+        ['/^[aefhilmnorsx]$/i', 0],
+        ['/^[bcdgjkpqtuvwyz]$/i', 1],
+        // abbreviations
+        [
+            '/^((?! FJO | [HLMNS]Y.  | RY[EO] | SQU | ( F[LR]? | [HL] | MN? | N | RH? | S[CHKLMNPTVW]? | X(YL)?) [AEIOU]) [FHLMNRSX][A-Z])/x',
+            0
+        ],
+        ['/^[aefhilmnorsx][.-]/i', 0],
+        ['/^[a-z][.-]/i', 1],
+        // consonants
+        ['/^[^aeiouy]/i', 1],
+        // special vowel forms
+        ['/^e[uw]/i', 1],
+        ["/^onc?e\b/i", 1],
+        ['/^uni([^nmd]|mo)/i', 1],
+        ['/^ut[th]/i', 2],
+        ['/^u[bcfhjkqrst][aeiou]/i', 1],
+        // special capitals
+        ['/^U[NK][AIEO]?/', 1],
+        // vowels
+        ['/^[aeiou]/i', 0],
+        // before certain consonants, y implies an "i" sound
+        ['/^(y(b[lor]|cl[ea]|fere|gg|p[ios]|rou|tt))/i', 0],
+    ];
+
     /**
      * Alias of the A() static method
      *
@@ -34,7 +67,7 @@ class IndefiniteArticle
             return $input;
         }
 
-        $result = self::_indef_article($word);
+        $result = self::_indefinite_article($word);
 
         return $pre . $result . $post;
     }
@@ -46,99 +79,24 @@ class IndefiniteArticle
      *
      * @return string       Original word prefixed with 'a' or 'an'
      */
-    private static function _indef_article($word): string
+    private static function _indefinite_article($word): string
     {
-        // any number starting with an '8' uses 'an'
-        if (\preg_match("/^[8](\d+)?/", $word)) {
-            return "an $word";
-        }
-
         // numbers starting with a '1' are trickier, only use 'an' if there are
         // 3, 6, 9, … digits after the 11 or 18
         if (\preg_match("/^[1][1](\d+)?/", $word) || \preg_match("/^[1][8](\d+)?/", $word)) {
             if (\strlen(\preg_replace(array("/\s/", '/,/', "/\.(\d+)?/"), '', $word)) % 3 === 2) {
-                return "an $word";
+                return 'an' . ' ' . $word;
             }
         }
 
-        // ordinal forms
-        if (\preg_match('/^([bcdgjkpqtuvwyz]-?th)/i', $word)) {
-            return "a $word";
-        }
-
-        if (\preg_match('/^([aefhilmnorsx]-?th)/i', $word)) {
-            return "an $word";
-        }
-
-        // special cases
-        if (\preg_match('/^(euler|hour(?!i)|heir|honest|hono)/i', $word)) {
-            return "an $word";
-        }
-
-        if (\preg_match('/^[aefhilmnorsx]$/i', $word)) {
-            return "an $word";
-        }
-
-        if (\preg_match('/^[bcdgjkpqtuvwyz]$/i', $word)) {
-            return "a $word";
-        }
-
-        // abbreviations
-        if (\preg_match('/^((?! FJO | [HLMNS]Y.  | RY[EO] | SQU | ( F[LR]? | [HL] | MN? | N | RH? | S[CHKLMNPTVW]? | X(YL)?) [AEIOU]) [FHLMNRSX][A-Z])/x',
-            $word)) {
-            return "an $word";
-        }
-
-        if (\preg_match('/^[aefhilmnorsx][.-]/i', $word)) {
-            return "an $word";
-        }
-
-        if (\preg_match('/^[a-z][.-]/i', $word)) {
-            return "a $word";
-        }
-
-        // consonants
-        if (\preg_match('/^[^aeiouy]/i', $word)) {
-            return "a $word";
-        }
-
-        // special vowel forms
-        if (\preg_match('/^e[uw]/i', $word)) {
-            return "a $word";
-        }
-
-        if (\preg_match("/^onc?e\b/i", $word)) {
-            return "a $word";
-        }
-
-        if (\preg_match('/^uni([^nmd]|mo)/i', $word)) {
-            return "a $word";
-        }
-
-        if (\preg_match('/^ut[th]/i', $word)) {
-            return "an $word";
-        }
-
-        if (\preg_match('/^u[bcfhjkqrst][aeiou]/i', $word)) {
-            return "a $word";
-        }
-
-        // special capitals
-        if (\preg_match('/^U[NK][AIEO]?/', $word)) {
-            return "a $word";
-        }
-
-        // vowels
-        if (\preg_match('/^[aeiou]/i', $word)) {
-            return "an $word";
-        }
-
-        // before certain consonants, y implies an "i" sound
-        if (\preg_match('/^(y(b[lor]|cl[ea]|fere|gg|p[ios]|rou|tt))/i', $word)) {
-            return "an $word";
+        foreach (self::$rules as $rule) {
+            [$pattern, $article] = $rule;
+            if (\preg_match($pattern, $word)) {
+                return ( $article ? 'a' : 'an') . ' ' . $word;
+            }
         }
 
         // not sure, so guess 'a'
-        return "a $word";
+        return 'a' . ' ' . $word;
     }
 }
